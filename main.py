15f0abe35eb6174d9579185258d38c3ac88395ce
Felicity <felicity.curreem@gmail.com>
2025-01-18 11:54:21 +0800

    L2

--- main.py
+++ main.py
@@ -5,12 +5,28 @@ Q1:
 
 # TODO: Write your code here
 
+print('Q1:')
+for i in range(7):
+    for j in range(i + 1):
+        print(j, end=' ')
+    print()
+
+print('#' * 30)
+
 ##################################################
 '''
 Q2:
 '''
 
 # TODO: Write your code here
+
+print('Q2:')
+for i in range(6):
+    for j in range(i+1):
+        print(j+1, end=' ')
+    print()
+
+print('#' * 30)
 
 ##################################################
 '''
@@ -19,6 +35,13 @@ Q3:
 
 # TODO: Write your code here
 
+print('Q3:')
+
+for i in range(9, 0, -1):
+    for j in range(5):
+        print(i, end = '')
+print()
+print('#' * 30)
 ##################################################
 '''
 Q4:
@@ -26,6 +49,14 @@ Q4:
 
 # TODO: Write your code here
 
+print('Q4:')
+
+for i in range(9, 0, -1):
+    for j in range(i):
+        print(i, end = '')
+
+print()
+print('#' * 30)
 ##################################################
 '''
 Q5:
@@ -33,6 +64,17 @@ Q5:
 
 # TODO: Write your code here
 
+print('Q5:')
+
+for i in range (5, 0, -1):
+    for j in range (i):
+        print(' ', end = '')    
+    for k in range((5-i)*2 +1):
+        print((5-i)*2 +1, end = '')
+    print()
+
+print()
+print('#' * 30)
 ##################################################
 '''
 Q6:
@@ -40,4 +82,17 @@ Q6:
 
 # TODO: Write your code here
 
+print('Q6:')
+
+for i in range (6, 0, -1):
+    for j in range (i):
+        print(' ', end = '')    
+    for k in range(1, (5-i) +1):
+        print(k, end = '')
+    for l in range((5-i)+1, 0, -1):
+        print(l, end='')
+    print()
+
+print()
+print('#' * 30)
 ##################################################
